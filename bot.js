e760d9af16394ab7400021f75e453304a39dc6a4
BradenTheProgrammer <45321716+BradenTheProgrammer@users.noreply.github.com>
2018-11-24 17:54:37 -0600

    bot.js
    not finished. not at my house, when I get back it'll be better.

---
+++ bot.js
@@ -0,0 +1,16 @@
+# This logs the bot in and connects in to th Discord API.
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
+# Insert your bot's token below.
+client.login('token');
